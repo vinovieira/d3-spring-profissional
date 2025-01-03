d08e53e6a792c1e08f10602240dd53104028dd05
vinovieira <127680187+vinovieira@users.noreply.github.com>
2025-01-02 21:57:49 -0400

    projeto inicial

---
+++ src/main/java/com/silvinovieira/d3_spring_profissional/D3SpringProfissionalApplication.java
@@ -0,0 +1,13 @@
+package com.silvinovieira.d3_spring_profissional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D3SpringProfissionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D3SpringProfissionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silvinovieira/d3_spring_profissional/D3SpringProfissionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silvinovieira.d3_spring_profissional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D3SpringProfissionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
