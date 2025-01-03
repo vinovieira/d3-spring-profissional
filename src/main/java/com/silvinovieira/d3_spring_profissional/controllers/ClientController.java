aca5bda175a411b75e90233e1867c91b63315ded
vinovieira <127680187+vinovieira@users.noreply.github.com>
2025-01-03 17:25:19 -0400

    implement pageable GET clients

--- src/main/java/com/silvinovieira/d3_spring_profissional/controllers/ClientController.java
+++ src/main/java/com/silvinovieira/d3_spring_profissional/controllers/ClientController.java
@@ -3,11 +3,15 @@ package com.silvinovieira.d3_spring_profissional.controllers;
 import com.silvinovieira.d3_spring_profissional.dto.ClientDTO;
 import com.silvinovieira.d3_spring_profissional.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(value = "/client")
+@RequestMapping(value = "/clients")
 public class ClientController {
 
     @Autowired
@@ -19,5 +23,10 @@ public class ClientController {
         return ResponseEntity.ok().body(dto);
     }
 
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> dto = clientService.findAll(pageable);
+        return ResponseEntity.ok().body(dto);
+    }
 
 }
--- src/main/java/com/silvinovieira/d3_spring_profissional/services/ClientService.java
+++ src/main/java/com/silvinovieira/d3_spring_profissional/services/ClientService.java
@@ -5,8 +5,12 @@ import com.silvinovieira.d3_spring_profissional.entities.Client;
 import com.silvinovieira.d3_spring_profissional.repositories.ClientRepositories;
 import com.silvinovieira.d3_spring_profissional.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -17,5 +21,11 @@ public class ClientService {
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("recurso não encontrado"));
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> clients = repository.findAll(pageable);
+        return clients.map(x -> new ClientDTO(x));
     }
 }
